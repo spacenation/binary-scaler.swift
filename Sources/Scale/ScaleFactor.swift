d386a774b9f3ffc1607ce35559de8e3934ec1329
Alex <alex.buzov@icloud.com>
2020-05-03 14:24:19 -0700

    scaling up

--- Sources/Scale/ScaleFactor.swift
+++ Sources/Scale/ScaleFactor.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 extension FixedWidthInteger where Self: UnsignedInteger {
-    public func scaling(factor: Self) -> Self {
+    public func scalingDown(factor: Self) -> Self {
         let portion: Float = Float(factor) / Float(Self.max)
         return .init(Float(self) * portion)
+    }
+    
+    public func scalingUp(factor: Self) -> Self {
+        let portion: Float = Float(Self.max) / Float(factor)
+        return Self.init(Swift.min(Float(self) * portion, Float(Self.max)))
     }
 }
--- Tests/ScaleTests/ScaleFactorTests.swift
+++ Tests/ScaleTests/ScaleFactorTests.swift
@@ -2,14 +2,22 @@ import XCTest
 import Scale
 
 final class ScaleFactorTests: XCTestCase {
-    func testScaleFactor() {
-        XCTAssert(UInt8(255).scaling(factor: 255) == 255)
-        XCTAssert(UInt8(255).scaling(factor: 127) == 127)
-        XCTAssert(UInt8(64).scaling(factor: 128) == 32)
-        XCTAssert(UInt8(255).scaling(factor: 0) == 0)
+    func testScaleDownFactor() {
+        XCTAssert(UInt8(255).scalingDown(factor: 255) == 255)
+        XCTAssert(UInt8(255).scalingDown(factor: 127) == 127)
+        XCTAssert(UInt8(64).scalingDown(factor: 128) == 32)
+        XCTAssert(UInt8(255).scalingDown(factor: 0) == 0)
+    }
+    
+    func testScaleUpFactor() {
+        XCTAssert(UInt8(255).scalingUp(factor: 255) == 255)
+        XCTAssert(UInt8(127).scalingUp(factor: 127) == 255)
+        XCTAssert(UInt8(32).scalingUp(factor: 127) == 64)
+        XCTAssert(UInt8(255).scalingUp(factor: 0) == 255)
     }
 
     static var allTests = [
-        ("testScaleFactor", testScaleFactor),
+        ("testScaleDownFactor", testScaleDownFactor),
+        ("testScaleUpFactor", testScaleUpFactor),
     ]
 }
