3ebd3169cf4da8303720f25119624166b101427d
Alex <alex.buzov@icloud.com>
2020-08-15 13:51:52 -0700

    renaming (#7)

--- Sources/Scale/FloatingPointScalable/UnsignedIntegers+FloatingPointScalable.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension UInt8: FloatingPointScalable {}
-extension UInt16: FloatingPointScalable {}
-extension UInt32: FloatingPointScalable {}
-extension UInt64: FloatingPointScalable {}
--- Tests/BinaryScalerTests/ScaleFactorTests.swift
+++ Tests/BinaryScalerTests/ScaleFactorTests.swift
@@ -1,7 +1,24 @@
 import XCTest
-import Scale
+import BinaryScaler
 
-final class ScaleFactorTests: XCTestCase {
+final class BinaryScalerTests: XCTestCase {
+    func testBinaryScaling() {
+        /// Int to Int
+        XCTAssert(%UInt8(255) == UInt8(255))
+        XCTAssert(%UInt16(65535) == UInt8(255))
+        XCTAssert(%UInt8(255) == UInt16(65280))
+        XCTAssert(%UInt32(4294967295) == UInt8(255))
+        XCTAssert(%UInt64(18446744073709551615) == UInt8(255))
+        
+        /// Float to Int
+        XCTAssert(%Float(0) == UInt16(0))
+        XCTAssert(%Float(1.0) == UInt8(255))
+        XCTAssert(%Double(1.0) == UInt16(65535))
+        /// Int to Float
+        XCTAssert(%UInt8(255) == Float(1.0))
+        XCTAssert(%UInt8(0) == Float(0))
+    }
+    
     func testScaleDownFactor() {
         XCTAssert(UInt8(255).scalingDown(factor: 255) == 255)
         XCTAssert(UInt8(255).scalingDown(factor: 127) == 127)
@@ -18,6 +35,7 @@ final class ScaleFactorTests: XCTestCase {
     }
 
     static var allTests = [
+        ("testBinaryScaling", testBinaryScaling),
         ("testScaleDownFactor", testScaleDownFactor),
         ("testScaleUpFactor", testScaleUpFactor),
     ]
